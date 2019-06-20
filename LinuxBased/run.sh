b59080e5a9c812ee1c54c3382bc22ee468265572
Alexey Koltsov <akoltsov@viacode.com>
2019-06-20 15:23:40 +0300

    Splitted organization and build agents parameters

--- LinuxBased/run.sh
+++ LinuxBased/run.sh
@@ -3,9 +3,10 @@
 HOSTNAME=$(hostname --fqdn | awk '{print tolower($0)}')
 
 JSON=$(cat $1)
+ORGS=$(cat ./../data/organizations.json)
 ACCOUNT=$(echo ${JSON} | jq -r .AzDOaccount.accountname)
 POOL=$(echo ${JSON} | jq -r .AzDOaccount.poolname)
-TOKEN=$(echo ${JSON} | jq -r .AzDOaccount.accesstoken)
+TOKEN=$(echo ${ORGS} | jq -r .${ACCOUNT}.accesstoken)
 AGENT=$(echo ${JSON} | jq -r .AzDOaccount.agentname)
 IMAGE=$(echo ${JSON} | jq -r .image)
 
@@ -16,6 +17,6 @@ docker run -d \
     -e VSTS_ACCOUNT=${ACCOUNT} \
     -e VSTS_POOL=${POOL} \
     -e VSTS_TOKEN=${TOKEN} \
-    -e VSTS_AGENT=${AGENT} \
+    -e VSTS_AGENT="${AGENT}-${POOL}" \
     -v /var/run/docker.sock:/var/run/docker.sock \
     "${HOSTNAME}/${IMAGE}"
