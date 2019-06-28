80be40db1c6d08f8609a45ee99e0c5fbc548a006
Alexey Koltsov <akoltsov@viacode.com>
2019-06-28 23:36:37 +0300

    Added basic files for testing

---
+++ LinuxBased/build.ps1
@@ -0,0 +1,55 @@
+[CmdletBinding(DefaultParameterSetName="JSON")]
+param(
+    [Parameter(Mandatory=$true,ParameterSetName="TextInput")]
+        [string]$BaseImage,
+    [Parameter(Mandatory=$true,ParameterSetName="TextInput")]
+        [string]$CustomImageName,
+    [Parameter(ParameterSetName="TextInput")]
+        [string]$Hostname,
+    [Parameter(ParameterSetName="TextInput")]
+        [string]$CustomTag,
+
+    [Parameter(ParameterSetName="JSON")]
+        [string]$ParamsJSON
+)
+
+# Parameters has been inputed manually
+if ($PsCmdlet.ParameterSetName -eq "TextInput") {
+
+}
+elseif ($PsCmdlet.ParameterSetName -eq "JSON") {
+    $InputParams = Get-Content $ParamsJSON | ConvertFrom-Json
+
+    $BaseImage = $InputParams.baseimage
+    $Hostname = $InputParams.hostname
+    $CustomImageName = $InputParams.customimagename
+    $CustomTag = $InputParams.customimagetag
+
+    if(-not $BaseImage){ throw "BaseImage is not defined" }
+    if(-not $CustomImageName){ throw "CustomImageName is not defined" }
+
+}
+else {
+    throw "Unsupported parameter set used"
+}
+
+if (-not $CustomTag) {
+    $CustomTag = "latest"
+}
+if (-not $Hostname) {
+    if (-not $env:UserDNSDomain) {
+        $Hostname = $env:ComputerName
+    }
+    else{
+        $Hostname = "$env:ComputerName.$env:UserDNSDomain"
+    }
+}
+$Hostname = $Hostname.ToLower()
+
+$FullCustomImageName = "$Hostname/${CustomImageName}:$CustomTag"
+
+Write-Output "Building image:`n Image name: ${FullCustomImageName}`n From: ${BaseImage}"
+docker build -t "${FullCustomImageName}" --build-arg baseImage=${BaseImage} -f Dockerfile .
+if ($CustomTag -ne "latest") {
+    docker tag "${FullCustomImageName}" "${Hostname}/${CustomImageName}:latest"
+}
