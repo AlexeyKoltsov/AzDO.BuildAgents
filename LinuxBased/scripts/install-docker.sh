b085590b0a59af0ce313c08db8546231c587a920
Alexey Koltsov <akoltsov@viacode.com>
2019-07-02 19:19:18 +0300

    Optimize Dockerfile and dependencies

---
+++ LinuxBased/scripts/install-docker.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt-get update
+apt-get install docker-ce
