23e4998b0faf232c911f07e98a8906e4a0445fe4
Alexey Koltsov <akoltsov@viacode.com>
2019-07-04 20:03:37 +0300

    Deleted agent removing via AzDO API

--- LinuxBased/run.ps1
+++ LinuxBased/run.ps1
@@ -8,9 +8,40 @@ function Remove-DockerContainer {
     param (
         [string]$Hash
     )
-    [void] (docker stop $Hash)
-    Start-Sleep 10
-    [void] (docker rm $Hash)
+    [void] ((docker stop $Hash) -and (docker rm $Hash))
+}
+
+#At this time supports only requests without body
+function Invoke-AzDOApi {
+    param (
+        [string]$APIver = "api-version=5.1-preview.1",
+        [string]$Account = $script:Account,
+        [string]$Token = $script:Token,
+        [string]$Method,
+        [string]$Uri
+    )
+    $Tokenbase64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "",$Token)))
+
+    Invoke-RestMethod -Method $Method -Uri $Uri -Headers @{Authorization=("Basic {0}" -f $Tokenbase64AuthInfo)}
+}
+
+function Remove-AzDOAgent {
+    param (
+        [string]$AgentName
+    )
+    $BaseAPIURI = "https://dev.azure.com/$Account/_apis"
+
+    #Getting ID of the Pool
+    $URIPool = "${BaseAPIURI}/distributedtask/pools?poolName=${script:Pool}&${APIver}"
+    $poolid = (Invoke-AzDOApi -Method Get -Uri $URIPool).value.id
+
+    #Getting ID of the Agent
+    $URIAgent = "$BaseAPIURI/distributedtask/pools/${poolid}/agents?agentName=${AgentName}&${APIver}"
+    $agentId = (Invoke-AzDOApi -Method Get -Uri $URIAgent).value.id
+
+    #Deleting the agent
+    $URIDelete = "$BaseAPIURI/distributedtask/pools/${poolid}/agents/${agentId}?$APIver"
+    Invoke-AzDOApi -Method Delete -Uri $URIDelete
 }
 
 $Kind = "linux"
@@ -61,7 +92,7 @@ $JSONData | % {
 "@
 
     $ExistingContainers = @()
-    $ExistingContainersRaw = $((docker ps -f name=$ContainerNameFamily --format '{{.ID}};{{.Image}};{{.Status}};{{.Names}}'))
+    $ExistingContainersRaw = $((docker ps -a -f name=$ContainerNameFamily --format '{{.ID}};{{.Image}};{{.Status}};{{.Names}}'))
     $ExistingContainersRaw |%{
         $item = $_ -split ';'
         $ExistingContainers += [PSCustomObject]@{
@@ -94,16 +125,15 @@ $JSONData | % {
         if ($i -gt $PoolSize) {
             Write-Output "Removing exceeding pool size container $($ExistingContainer.name)"
             Remove-DockerContainer -Hash $ExistingContainer.hash
+            Remove-AzDOAgent -AgentName $ExistingContainer.name
             continue
         }
-
-
-        $Expression = $ExpressionTpl -replace "{{num}}", $NumReplacer
-
+        
         if ($ExistingContainer) {
             if ($Override) {
                 Write-Output "Overriding $($ExistingContainer.name)"
                 Remove-DockerContainer -Hash $ExistingContainer.hash
+                Remove-AzDOAgent -AgentName $ExistingContainer.name
 
             }
             else{
@@ -112,6 +142,8 @@ $JSONData | % {
             }
             
         }
+
+        $Expression = $ExpressionTpl -replace "{{num}}", $NumReplacer
 
         Write-Output "`n===================="
         Write-Output "Running image:`n Image name: ${FullCustomImage}`n Agent name: ${ContainerNameReplaced}`n Pool: ${Pool}"
